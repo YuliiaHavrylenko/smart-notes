bf5fc38af1b52cc1a66fc7337757d2bbc66e15c9
Yuliia Havrylenko <volttabaki@gmail.com>
2023-02-26 10:11:18 -0800

    Initial commit

---
+++ notes/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+
+from .models import Notes
+from .forms import NotesForm
+
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+
+class NotesCreateView(CreateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
